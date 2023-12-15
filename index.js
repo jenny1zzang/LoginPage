5fb21f6e33b68d9c2870190a15526238d1848657
jenny1zzang <cyberkhl@naver.com>
2023-12-15 13:34:00 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { User } = require("./models/user")
+const config = require('./config/key')
 
-mongoose.connect('mongodb+srv://jenny1zzang:0627herinkim@boilerplate.mqii1xe.mongodb.net/')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello World hi')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(200).json({
+            success: true
+        })
+    }).catch((err) => {
+        return res.json({ success: false, err })
+    })
 })
 
 app.listen(3000)
